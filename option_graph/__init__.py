8b03499dd70926ec63f299f571c6ced92f3f142f
Mathïs Fédérico <mathfederico@gmail.com>
2021-07-22 17:37:09 +0200

    Updated __init__ with main objects

--- option_graph/__init__.py
+++ option_graph/__init__.py
@@ -1,2 +1,8 @@
 # OptionGraph for explainable hierarchical reinforcement learning
 # Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+""" A structure for explainable hierarchical reinforcement learning """
+
+from option_graph.option import Option
+from option_graph.option_graph import OptionGraph
+from option_graph.feature_condition import FeatureCondition
