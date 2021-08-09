55ea685460c918a59667cf884194528745732472
Mathïs Fédérico <mathfederico@gmail.com>
2021-08-09 09:07:58 +0200

    :wrench: Refactor tests for paper examples

--- tests/integration/metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-# OptionGraph for explainable hierarchical reinforcement learning
-# Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
-
-""" Integration tests for option_graph.metrics submodules. """
--- tests/integration/test_paper_basic_example.py
+++ tests/integration/test_paper_basic_example.py
@@ -1,16 +1,17 @@
 # OptionGraph for explainable hierarchical reinforcement learning
 # Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-""" Integration tests for the option_graph.metrics.complexity.histograms module. """
+""" Integration tests for the initial paper examples. """
 
 import pytest
 import pytest_check as check
 
 from option_graph.metrics.complexity.histograms import nodes_histograms
+from option_graph.metrics.complexity.complexities import learning_complexity
 from option_graph import Action, Option, FeatureCondition, OptionGraph
 
-class TestHistograms:
-    """Histograms"""
+class TestPaperBasicExamples:
+    """Basic examples from the initial paper"""
 
     @pytest.fixture(autouse=True)
     def setup(self):
@@ -61,9 +62,41 @@ class TestHistograms:
         self.actions = [Action(i) for i in range(3)]
         self.feature_conditions = [FeatureCondition(f"feature {i}") for i in range(6)]
         self.options = [Option0(), Option1(), Option2()]
+        self.used_nodes_all = {
+            self.options[0]: {
+                self.actions[0]: 1,
+                self.actions[1]: 1,
+                self.options[0]: 1,
+                self.feature_conditions[0]: 1,
+            },
+            self.options[1]: {
+                self.actions[0]: 2,
+                self.actions[1]: 1,
+                self.actions[2]: 1,
+                self.options[0]: 1,
+                self.options[1]: 1,
+                self.feature_conditions[0]: 1,
+                self.feature_conditions[1]: 1,
+                self.feature_conditions[2]: 1,
+            },
+            self.options[2]: {
+                self.actions[0]: 6,
+                self.actions[1]: 3,
+                self.actions[2]: 2,
+                self.options[0]: 3,
+                self.options[1]: 2,
+                self.options[2]: 1,
+                self.feature_conditions[0]: 3,
+                self.feature_conditions[1]: 2,
+                self.feature_conditions[2]: 2,
+                self.feature_conditions[3]: 1,
+                self.feature_conditions[4]: 1,
+                self.feature_conditions[5]: 1,
+            },
+        }
 
     def test_get_used_nodes(self):
-        """'nodes_histograms' should give expected results on the paper handcrafted example. """
+        """should give expected nodes_histograms results. """
 
         expected_used_nodes_all = {
             self.options[0]: {
@@ -100,3 +133,26 @@ class TestHistograms:
 
         used_nodes_all = nodes_histograms(self.options)
         check.equal(used_nodes_all, expected_used_nodes_all)
+
+    def test_learning_complexity_example(self):
+        """should give expected learning_complexity. """
+        expected_learning_complexities = {
+            self.options[0]: 3,
+            self.options[1]: 7,
+            self.options[2]: 11,
+        }
+        expected_saved_complexities = {
+            self.options[0]: 0,
+            self.options[1]: 0,
+            self.options[2]: 10,
+        }
+
+        for option in self.options:
+            c_learning, saved_complexity = learning_complexity(option,
+                used_nodes_all=self.used_nodes_all)
+
+            print(f"{option}: {c_learning}|{expected_learning_complexities[option]}"
+                  f" {saved_complexity}|{expected_saved_complexities[option]}")
+            check.equal(c_learning, expected_learning_complexities[option])
+            check.equal(saved_complexity, expected_saved_complexities[option])
+
