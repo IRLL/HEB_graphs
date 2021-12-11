873d61106817a2654a46846bef9fa003ade314d8
Mathïs Fédérico <mathfederico@gmail.com>
2021-12-11 11:49:51 +0100

    :beetle: Fix _compute_level_dependencies

--- option_graph/graph.py
+++ option_graph/graph.py
@@ -8,8 +8,9 @@ from networkx import DiGraph
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from matplotlib.axes import Axes
 
-def get_roots(graph:DiGraph):
-    """ Finds roots in a DiGraph.
+
+def get_roots(graph: DiGraph):
+    """Finds roots in a DiGraph.
 
     Args:
         graph: A networkx DiGraph.
@@ -24,21 +25,23 @@ def get_roots(graph:DiGraph):
             roots.append(node)
     return roots
 
-def get_nodes_by_level(graph:DiGraph):
+
+def get_nodes_by_level(graph: DiGraph):
     nodes_by_level = {}
     for node in graph.nodes():
-        level = graph.nodes[node]['level']
+        level = graph.nodes[node]["level"]
         try:
             nodes_by_level[level].append(node)
         except KeyError:
             nodes_by_level[level] = [node]
 
-    graph.graph['nodes_by_level'] = nodes_by_level
-    graph.graph['depth'] = max(level for level in nodes_by_level)
+    graph.graph["nodes_by_level"] = nodes_by_level
+    graph.graph["depth"] = max(level for level in nodes_by_level)
     return nodes_by_level
 
-def compute_levels(graph:DiGraph):
-    """ Compute the hierachical levels of all DiGraph nodes.
+
+def compute_levels(graph: DiGraph):
+    """Compute the hierachical levels of all DiGraph nodes.
 
     Adds the attribute 'level' to each node in the given graph.
     Adds the attribute 'nodes_by_level' to the given graph.
@@ -52,42 +55,57 @@ def compute_levels(graph:DiGraph):
 
     """
 
-    def _compute_level_dependencies(graph:DiGraph, node):
+    def _compute_level_dependencies(graph: DiGraph, node):
         predecessors = list(graph.predecessors(node))
         if len(predecessors) == 0:
-            graph.nodes[node]['level'] = 0
+            graph.nodes[node]["level"] = 0
             return True
 
         pred_level_by_index = {}
         for pred in predecessors:
-            index = graph.edges[pred, node]['index']
+            index = graph.edges[pred, node]["index"]
             try:
-                pred_level = graph.nodes[pred]['level']
+                pred_level = graph.nodes[pred]["level"]
             except KeyError:
-                return False
+                pred_level = None
 
             if index in pred_level_by_index:
                 pred_level_by_index[index].append(pred_level)
             else:
                 pred_level_by_index[index] = [pred_level]
 
-        min_level_by_index = [min(level_list) for _, level_list in pred_level_by_index.items()]
-        level =  1 + max(min_level_by_index)
-        graph.nodes[node]['level'] = level
+        min_level_by_index = []
+        for index, level_list in pred_level_by_index.items():
+            level_list_wo_none = [l for l in level_list if l is not None]
+            if len(level_list_wo_none) == 0:
+                return False
+            min_level_by_index.append(min(level_list_wo_none))
+        level = 1 + max(min_level_by_index)
+        graph.nodes[node]["level"] = level
         return True
 
-    all_nodes_have_level = False
-    while not all_nodes_have_level:
+    for _ in range(len(graph.nodes())):
         all_nodes_have_level = True
+        incomplete_nodes = []
         for node in graph.nodes():
             incomplete = not _compute_level_dependencies(graph, node)
             if incomplete:
+                incomplete_nodes.append(node)
                 all_nodes_have_level = False
+        if all_nodes_have_level:
+            break
+
+    if not all_nodes_have_level:
+        raise ValueError(
+            "Could not attribute levels to all nodes. "
+            f"Incomplete nodes: {incomplete_nodes}"
+        )
 
     return get_nodes_by_level(graph)
 
-def compute_edges_color(graph:DiGraph):
-    """ Compute the edges colors of a leveled graph for readability.
+
+def compute_edges_color(graph: DiGraph):
+    """Compute the edges colors of a leveled graph for readability.
 
     Adds the attribute 'color' and 'linestyle' to each edge in the given graph.
     Nodes with a lot of successors will have more transparent edges.
@@ -103,16 +121,17 @@ def compute_edges_color(graph:DiGraph):
         successors = list(graph.successors(node))
         for succ in successors:
             alpha = 0.2
-            if graph.nodes[node]['level'] < graph.nodes[succ]['level']:
+            if graph.nodes[node]["level"] < graph.nodes[succ]["level"]:
                 if len(successors) < len(alphas):
-                    alpha = alphas[len(successors)-1]
+                    alpha = alphas[len(successors) - 1]
             else:
-                graph.edges[node, succ]['linestyle'] = "dashed"
-            if isinstance(graph.edges[node, succ]['color'], list):
-                graph.edges[node, succ]['color'][3] = alpha
+                graph.edges[node, succ]["linestyle"] = "dashed"
+            if isinstance(graph.edges[node, succ]["color"], list):
+                graph.edges[node, succ]["color"][3] = alpha
 
-def draw_networkx_nodes_images(graph:DiGraph, pos, ax:Axes, img_zoom:float=1):
-    """ Draw nodes images of a networkx DiGraph on a given matplotlib ax.
+
+def draw_networkx_nodes_images(graph: DiGraph, pos, ax: Axes, img_zoom: float = 1):
+    """Draw nodes images of a networkx DiGraph on a given matplotlib ax.
 
     Args:
         graph: A networkx DiGraph.
@@ -122,19 +141,18 @@ def draw_networkx_nodes_images(graph:DiGraph, pos, ax:Axes, img_zoom:float=1):
 
     """
     for n in graph:
-        img = graph.nodes[n]['image']
-        color = graph.nodes[n]['color']
+        img = graph.nodes[n]["image"]
+        color = graph.nodes[n]["color"]
         if img is not None:
             min_dim = min(img.shape[:2])
             min_ax_shape = min(ax._position.width, ax._position.height)
             zoom = 100 * img_zoom * min_ax_shape / min_dim
             imagebox = OffsetImage(img, zoom=zoom)
             imagebox = AnnotationBbox(
-                imagebox, pos[n],
-                frameon=True, box_alignment=(0.5, 0.5)
+                imagebox, pos[n], frameon=True, box_alignment=(0.5, 0.5)
             )
 
-            imagebox.patch.set_facecolor('None')
+            imagebox.patch.set_facecolor("None")
             imagebox.patch.set_edgecolor(color)
             imagebox.patch.set_linewidth(3)
             imagebox.patch.set_boxstyle("round", pad=0.15)
