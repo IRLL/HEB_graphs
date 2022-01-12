6dde6ec55dde6db094359f17576d13e273572ffa
Mathïs Fédérico <mathfederico@gmail.com>
2022-01-12 16:53:13 +0100

    :beetle: Fix Simulated annealing display

--- option_graph/layouts/metaheuristics.py
+++ option_graph/layouts/metaheuristics.py
@@ -5,11 +5,18 @@
 
 import numpy as np
 
-def simulated_annealing(initial, energy, neighbor,
-    max_iterations:int=1000, initial_temperature:float=5,
-    max_iters_without_new:int=np.inf, verbose:bool=1):
 
-    """ Perform simulated annealing metaheuristic on an energy using a neighboring.
+def simulated_annealing(
+    initial,
+    energy,
+    neighbor,
+    max_iterations: int = 1000,
+    initial_temperature: float = 5,
+    max_iters_without_new: int = np.inf,
+    verbose: bool = 1,
+):
+
+    """Perform simulated annealing metaheuristic on an energy using a neighboring.
 
     See https://en.wikipedia.org/wiki/Simulated_annealing for more details.
 
@@ -24,7 +31,7 @@ def simulated_annealing(initial, energy, neighbor,
     """
 
     def prob_keep(temperature, delta_e):
-        return min(1, np.exp(delta_e/temperature))
+        return min(1, np.exp(delta_e / temperature))
 
     x = initial
     energy_pos = energy(x)
@@ -32,16 +39,17 @@ def simulated_annealing(initial, energy, neighbor,
     for k in range(max_iterations):
         new_x = neighbor(x)
         new_e = energy(new_x)
-        temperature = initial_temperature/(k+1)
+        temperature = initial_temperature / (k + 1)
         iters_without_new += 1
         prob = prob_keep(temperature, energy_pos - new_e)
         if np.random.random() < prob:
             if verbose == 1:
-                print(f"{k}\t({prob:.0%})\t{energy_pos:.2f}->{new_e:.2f}", end='\r')
+                print(f"{k}\t({prob:.0%})\t{energy_pos:.2f}->{new_e:.2f}", end="\r")
             x, energy_pos = new_x, new_e
             iters_without_new = 0
 
         if iters_without_new >= max_iters_without_new:
             break
-
+    if verbose == 1:
+        print()
     return x
