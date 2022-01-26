ae98b2e820520594a8deb365640b478d4a776225
Mathïs Fédérico <mathfederico@gmail.com>
2022-01-26 18:54:53 +0100

    :beetle: Fix tests were in package

--- setup.py
+++ setup.py
@@ -10,14 +10,20 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.rst").read_text()
 
+
 def get_version():
-    version_file = open('VERSION')
+    version_file = open("VERSION")
     return version_file.read().strip()
+
+
 VERSION = get_version()
 
+
 def get_requirements():
-    requirements_file = open('requirements.txt')
+    requirements_file = open("requirements.txt")
     return requirements_file.readlines()
+
+
 REQUIREMENTS = get_requirements()
 
 setup(
@@ -29,12 +35,12 @@ setup(
     long_description=README,
     long_description_content_type="text/x-rst",
     url="https://github.com/IRLL/options_graphs",
-    packages=find_packages(exclude=("tests", "docs")),
+    packages=find_packages(exclude=("tests*", "docs*")),
     install_requires=REQUIREMENTS,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
