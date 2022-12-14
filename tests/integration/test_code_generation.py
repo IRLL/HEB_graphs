8b1fcddd211c4330ebd522c9b83587a61d2d87c6
Mathïs Fédérico <mathfederico@gmail.com>
2022-12-14 17:16:48 +0100

    :red_circle: :umbrella: Add test_f_a_graph_codegen

--- tests/integration/test_code_generation.py
+++ tests/integration/test_code_generation.py
@@ -1,12 +1,16 @@
 import pytest_check as check
 
-from tests.integration import FundamentalBehavior
+from tests.integration import (
+    FundamentalBehavior,
+    ThresholdFeatureCondition,
+    F_A_Behavior,
+)
 from hebg.node import Action
 from hebg.codegen import get_hebg_source
 
 
 def test_a_graph_codegen():
-    """(A) Fundamental behaviors (single action) should generate __call__ content only."""
+    """(A) Fundamental behaviors (single Action node) should return action call."""
     action = Action(42)
     a_graph = FundamentalBehavior(action).graph
     source_code = get_hebg_source(a_graph)
@@ -19,4 +23,29 @@ def test_a_graph_codegen():
             "        return self.action_42(observation)",
         )
     )
+    check.equal(source_code, expected_source_code)
+
+
+def test_f_a_graph_codegen():
+    """(F-A) Feature condition should generate if/else condition."""
+    feature_condition = ThresholdFeatureCondition(relation=">=", threshold=0)
+    actions = {0: Action(0), 1: Action(1)}
+    behavior = F_A_Behavior("Is above_zero", feature_condition, actions)
+    f_a_graph = behavior.graph
+    source_code = get_hebg_source(f_a_graph)
+    expected_source_code = "\n".join(
+        (
+            "class IsAboveZero:",
+            "    def __init__(self):",
+            "        self.action_0 = Action(0)",
+            "        self.action_1 = Action(1)",
+            '        self.greater_or_equal_to_0 = ThresholdFeatureCondition(relation=">=", threshold=0)',
+            "    def __call__(self, observation):",
+            "        if self.greater_or_equal_to_0(observation) == 0",
+            "            return self.action_0(observation)",
+            "        if self.greater_or_equal_to_0(observation) == 1",
+            "            return self.action_1(observation)",
+        )
+    )
+
     check.equal(source_code, expected_source_code)
