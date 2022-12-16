6ce062ef95db3af11430c8f8d35fa8ea2d270fda
Mathïs Fédérico <mathfederico@gmail.com>
2022-12-16 13:48:56 +0100

    :wrench: :umbrella: Refactor tests to check code execution

--- tests/integration/test_code_generation.py
+++ tests/integration/test_code_generation.py
@@ -1,3 +1,6 @@
+from typing import Dict, Tuple, Any
+
+import pytest
 import pytest_check as check
 
 from tests.integration import (
@@ -6,55 +9,69 @@ from tests.integration import (
     F_A_Behavior,
     HEBGraph,
 )
-from hebg.node import Action
+from hebg.node import Action, FeatureCondition
 from hebg.behavior import Behavior
 from hebg.codegen import get_hebg_source
 
 
-def test_a_graph_codegen():
+class TestABehavior:
     """(A) Fundamental behaviors (single Action node) should return action call."""
-    action = Action(42)
-    graph = FundamentalBehavior(action).graph
-    source_code = get_hebg_source(graph)
-    expected_source_code = "\n".join(
-        (
-            "class Action42Behavior:",
-            "    def __init__(self, actions:Dict[str, Action], feature_conditions: Dict[str, FeatureCondition]):",
-            "        self.actions = actions",
-            "        self.feature_conditions = feature_conditions",
-            "    def __call__(self, observation):",
-            "        return self.actions['action 42'](observation)",
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.behavior = FundamentalBehavior(Action(42))
+
+    def test_source_codegen(self):
+        source_code = get_hebg_source(self.behavior.graph)
+        expected_source_code = "\n".join(
+            (
+                "class Action42Behavior:",
+                "    def __init__(self, actions:Dict[str, Action], feature_conditions: Dict[str, FeatureCondition]):",
+                "        self.actions = actions",
+                "        self.feature_conditions = feature_conditions",
+                "    def __call__(self, observation):",
+                "        return self.actions['action 42'](observation)",
+            )
         )
-    )
-    check.equal(source_code, expected_source_code)
+        check.equal(source_code, expected_source_code)
+
+    def test_exec_codegen(self):
+        check_execution_for_values(self.behavior, "Action42Behavior", (1, -1))
 
 
-def test_f_a_graph_codegen():
+class TestFABehavior:
     """(F-A) Feature condition should generate if/else condition."""
-    feature_condition = ThresholdFeatureCondition(relation=">=", threshold=0)
-    actions = {0: Action(0), 1: Action(1)}
-    behavior = F_A_Behavior("Is above_zero", feature_condition, actions)
-    graph = behavior.graph
-    source_code = get_hebg_source(graph)
-    expected_source_code = "\n".join(
-        (
-            "class IsAboveZero:",
-            "    def __init__(self, actions:Dict[str, Action], feature_conditions: Dict[str, FeatureCondition]):",
-            "        self.actions = actions",
-            "        self.feature_conditions = feature_conditions",
-            "    def __call__(self, observation):",
-            "        edge_index = self.feature_conditions['Greater or equal to 0 ?'](observation)",
-            "        if edge_index == 0:",
-            "            return self.actions['action 0'](observation)",
-            "        if edge_index == 1:",
-            "            return self.actions['action 1'](observation)",
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        feature_condition = ThresholdFeatureCondition(relation=">=", threshold=0)
+        actions = {0: Action(0), 1: Action(1)}
+        self.behavior = F_A_Behavior("Is above_zero", feature_condition, actions)
+
+    def test_source_codegen(self):
+        source_code = get_hebg_source(self.behavior.graph)
+        expected_source_code = "\n".join(
+            (
+                "class IsAboveZero:",
+                "    def __init__(self, actions:Dict[str, Action], feature_conditions: Dict[str, FeatureCondition]):",
+                "        self.actions = actions",
+                "        self.feature_conditions = feature_conditions",
+                "    def __call__(self, observation):",
+                "        edge_index = self.feature_conditions['Greater or equal to 0 ?'](observation)",
+                "        if edge_index == 0:",
+                "            return self.actions['action 0'](observation)",
+                "        if edge_index == 1:",
+                "            return self.actions['action 1'](observation)",
+            )
         )
-    )
 
-    check.equal(source_code, expected_source_code)
+        check.equal(source_code, expected_source_code)
+
+    def test_exec_codegen(self):
+        check_execution_for_values(self.behavior, "IsAboveZero", (1, -1))
 
 
-def test_f_f_a_graph_codegen():
+class TestFFABehavior:
     """(F-F-A) Chained FeatureConditions should condition should generate nested if/else."""
 
     class F_F_A_Behavior(Behavior):
@@ -79,30 +96,59 @@ def test_f_f_a_graph_codegen():
 
             return graph
 
-    behavior = F_F_A_Behavior("scalar classification ]-1,0,1[ ?")
-    graph = behavior.graph
-    source_code = get_hebg_source(graph)
-    expected_source_code = "\n".join(
-        (
-            "class ScalarClassification101:",
-            "    def __init__(self, actions:Dict[str, Action], feature_conditions: Dict[str, FeatureCondition]):",
-            "        self.actions = actions",
-            "        self.feature_conditions = feature_conditions",
-            "    def __call__(self, observation):",
-            "        edge_index = self.feature_conditions['Greater or equal to 0 ?'](observation)",
-            "        if edge_index == 0:",
-            "            edge_index_1 = self.feature_conditions['Lesser or equal to 1 ?'](observation)",
-            "            if edge_index_1 == 0:",
-            "                return self.actions['action 0'](observation)",
-            "            if edge_index_1 == 1:",
-            "                return self.actions['action 1'](observation)",
-            "        if edge_index == 1:",
-            "            edge_index_1 = self.feature_conditions['Greater or equal to -1 ?'](observation)",
-            "            if edge_index_1 == 0:",
-            "                return self.actions['action 2'](observation)",
-            "            if edge_index_1 == 1:",
-            "                return self.actions['action 3'](observation)",
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.behavior = self.F_F_A_Behavior("scalar classification ]-1,0,1[ ?")
+
+    def test_source_codegen(self):
+        source_code = get_hebg_source(self.behavior.graph)
+        expected_source_code = "\n".join(
+            (
+                "class ScalarClassification101:",
+                "    def __init__(self, actions:Dict[str, Action], feature_conditions: Dict[str, FeatureCondition]):",
+                "        self.actions = actions",
+                "        self.feature_conditions = feature_conditions",
+                "    def __call__(self, observation):",
+                "        edge_index = self.feature_conditions['Greater or equal to 0 ?'](observation)",
+                "        if edge_index == 0:",
+                "            edge_index_1 = self.feature_conditions['Lesser or equal to 1 ?'](observation)",
+                "            if edge_index_1 == 0:",
+                "                return self.actions['action 0'](observation)",
+                "            if edge_index_1 == 1:",
+                "                return self.actions['action 1'](observation)",
+                "        if edge_index == 1:",
+                "            edge_index_1 = self.feature_conditions['Greater or equal to -1 ?'](observation)",
+                "            if edge_index_1 == 0:",
+                "                return self.actions['action 2'](observation)",
+                "            if edge_index_1 == 1:",
+                "                return self.actions['action 3'](observation)",
+            )
         )
+
+        check.equal(source_code, expected_source_code)
+
+    def test_exec_codegen(self):
+        check_execution_for_values(
+            self.behavior, "ScalarClassification101", (2, 1, -1, -2)
+        )
+
+
+def check_execution_for_values(behavior: Behavior, class_name: str, values: Tuple[Any]):
+    exec(get_hebg_source(behavior.graph))
+    CodeGenPolicy = locals()[class_name]
+
+    actions = {
+        node.name: node for node in behavior.graph.nodes if isinstance(node, Action)
+    }
+    feature_conditions = {
+        node.name: node
+        for node in behavior.graph.nodes
+        if isinstance(node, FeatureCondition)
+    }
+
+    behavior_rebuilt = CodeGenPolicy(
+        actions=actions, feature_conditions=feature_conditions
     )
 
-    check.equal(source_code, expected_source_code)
+    for val in values:
+        check.equal(behavior(val), behavior_rebuilt(val))
